6b0f165631d0bce7eb90bf0e221735b51c5c75dc
Ebube Ireneaus <alfredebube7@gmail.com>
2025-12-19 15:15:29 +0100

    jbjjhjhjh

--- app/settings.py
+++ app/settings.py
@@ -52,8 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ]
 
