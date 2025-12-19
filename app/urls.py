ef5d7165e324cb85885550887bbe3e3c706cf613
Ebube Ireneaus <alfredebube7@gmail.com>
2025-12-19 15:40:47 +0100

    hello

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from .api import api
@@ -22,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',api.urls )
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
